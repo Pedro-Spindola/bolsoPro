e98cfe2462445bd5ea916932225e002157a73779
Pedro-Spindola <spindolapedrohenrique99@gmail.com>
2024-11-08 00:09:27 -0300

    Uptade 08/11

--- src/components/BoxPlanosConquista.jsx
+++ src/components/BoxPlanosConquista.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
 import styles from "./BoxPlanosConquista.module.css"
 
-const BoxPlanosConquista = ({ icone, titulo, meta, valorColetado, ValorRestante}) => {
+const BoxPlanosConquista = ({ icone, titulo, vMensal, valorColetado, valorRestante}) => {
   return (
     <div className={styles.container}>
         <img src={icone}></img>
         <div className={styles.infPlano}>
             <h1>{titulo}</h1>
-            <h2>Valor planejado: {meta}</h2>
+            <h3>Valor mensal: </h3>
+            <h2>{vMensal}</h2>
         </div>
         <div className={styles.progresso}>
             <h2>{valorColetado}</h2>
-            <h2>Restante: {ValorRestante}</h2>
+            <h2>{valorRestante}</h2>
             <div className={styles.barProgresso}></div>
         </div>
     </div>
@@ -20,9 +21,9 @@ const BoxPlanosConquista = ({ icone, titulo, meta, valorColetado, ValorRestante}
     BoxPlanosConquista.propTypes = {
         icone: PropTypes.node.isRequired,
         titulo: PropTypes.string.isRequired,
-        meta: PropTypes.string.isRequired,
+        vMensal: PropTypes.string.isRequired,
         valorColetado: PropTypes.string.isRequired,
-        ValorRestante: PropTypes.string.isRequired,
+        valorRestante: PropTypes.string.isRequired,
     };
 
 export default BoxPlanosConquista
--- src/router/Home.jsx
+++ src/router/Home.jsx
@@ -1,4 +1,8 @@
 import styles from "./Home.module.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCards } from 'swiper/modules';
+import "swiper/css"
+import "swiper/css/effect-cards"
 import carteira from "../image/moedas.png"
 import rendaMensal from "../image/lucros.png"
 import despesasMensal from "../image/perda.png"
@@ -33,20 +37,45 @@ const Home = () => {
         </div>
         <div className={styles.boxPlanejamentos}>
           <div className={styles.boxPlanosIndividual}>
-            <BoxPlanosConquista icone={carteira} titulo="PS5" meta="R$ 3.000,00" valorColetado="R$ 1.400,00" valorRestante="R$ 1.600,00" />
+            <BoxPlanosConquista icone={carteira} titulo="PS5" vMensal="R$ 120,00" valorColetado="R$ 1.100,00" valorRestante="R$ 600,00" />
+            <BoxPlanosConquista icone={carteira} titulo="Carro" vMensal="R$ 300,00" valorColetado="R$ 8.400,00" valorRestante="R$ 30.600,00" />
+            <BoxPlanosConquista icone={carteira} titulo="Computador Gamer" vMensal="R$ 200,00" valorColetado="R$ 3.600,00" valorRestante="R$ 1.600,00" />
+            <BoxPlanosConquista icone={carteira} titulo="New Zelândia" vMensal="R$ 400,00" valorColetado="R$ 11.800,00" valorRestante="R$ 10.600,00" />
           </div>
         </div>
         <div className={styles.boxTabelaUltimosLancamentos}>
-          
+        
         </div>
       </div>
       <div className={styles.container02}>
         <div className={styles.boxMeusCartoes}>
           <div className={styles.carrocelCartao}>
-            <Cartao className={styles.cartao} nomeCartao="NuBank" bandeira="Mastercard" nomeCompleto="Pedro Spíndola" ultimosNumero="0366" validadeAno="08" validadeMes="31" />
+            <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]} className={styles.swiper_container}>
+              <SwiperSlide className={styles.swiper_slide}><Cartao className={styles.cartao1} nomeCartao="NuBank" bandeira="Mastercard" nomeCompleto="Pedro Spíndola" ultimosNumero="0366" validadeAno="08" validadeMes="31" /></SwiperSlide>
+              <SwiperSlide className={styles.swiper_slide}><Cartao className={styles.cartao2} nomeCartao="Inter" bandeira="Mastercard" nomeCompleto="Pedro Spíndola" ultimosNumero="0485" validadeAno="12" validadeMes="29" /></SwiperSlide>
+              <SwiperSlide className={styles.swiper_slide}><Cartao className={styles.cartao3} nomeCartao="Mercado Pago" bandeira="Visa" nomeCompleto="Pedro Spíndola" ultimosNumero="4256" validadeAno="22" validadeMes="28" /></SwiperSlide>
+            </Swiper>
           </div>
           <div className={styles.informacaoCartao}>
+            <div className={styles.boxInforPadrao}>
+              <h3>Disponível</h3>
+              <h2>R$ 1.200,00</h2>
+            </div>
+            <div className={styles.boxInforPadrao}>
+              <h3>Fatura Atual</h3>
+              <h2>R$ 380,00</h2>
+            </div>
+            <div className={styles.boxInforPadrao}>
+              <h3>Data de fechamneto</h3>
+              <h2>12/05</h2>
+            </div>
+            <div className={styles.boxInforPadrao}>
+              <h3>Variação Mensal</h3>
+              <h2>2.45%</h2>
+            </div>
+            <div className={styles.graficoFaturaMensal}>
 
+            </div>
           </div>
         </div>
         <div className={styles.boxListaMaioresGastos}>
